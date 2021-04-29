93030351d257ee332d12df20d46b58db22ec235d
John Chandara <email@john.science>
2021-04-29 18:03:33 -0500

    [refactor] description text

--- resources/views/dd20/view.blade.php
+++ resources/views/dd20/view.blade.php
@@ -27,7 +27,7 @@
                 <form>
                     <div class="form-group">
                         <label>Points accumulated</label>
-                        <div class="col-sm-6">
+                        <div class="col-sm-2">
                             <input class="form-control" type="text" placeholder="Default input">
                         </div>
                     </div>
@@ -35,23 +35,30 @@
             </div>
         </div>
 
-        <h2>Missions</h2>
+        <h2>Campaign Missions</h2>
+        <i class="small">All missions completed by the player, grouped by map.</i>
         <div id="accordion">
-            @foreach(['mvm_autumnnull', 'mvm_example', 'mvm_bigrock'] as $map)
+            @foreach(['mvm_autumnull_rc2', 'mvm_example', 'mvm_bigrock'] as $map)
                 <div class="card">
                     <div class="card-header" id="heading">
-                        <div class="row">
-                            <div class="col col-1">
-                                <img class="w-100 h-100" src="//creators.tf/api/mapthumb?map={{$map}}"/>
+                        <div class="d-flex justify-content-between align-items-center">
+                            <div class="d-flex flex-grow-1 align-items-center">
+                                <div class="col col-1">
+                                    <img class="img-thumbnail w-100 h-100" src="//creators.tf/api/mapthumb?map={{$map}}"/>
+                                </div>
+                                <button class="btn btn-link" data-toggle="collapse" data-target="#collapse" aria-expanded="true" aria-controls="collapse">
+                                    <h2>{{ucfirst(explode('_', $map)[1])}}</h2>
+                                </button>
+                                <span class="text-muted">2 mission available</span>
                             </div>
-                            <button class="btn btn-link" data-toggle="collapse" data-target="#collapse" aria-expanded="true" aria-controls="collapse">
-                                <h2>{{$map}}</h2>
-                            </button>
+                            <div>
+                                <h2 class="fas fa-chevron-down"></h2>
+                            </div>
                         </div>
                     </div>
                     <div id="collapse" class="collapse" aria-labelledby="heading" data-parent="#accordion">
                         <div class="card-body">
-                            @foreach($missions as $mission)
+                            @foreach($missions as $i => $mission)
                                 <form>
                                     <div class="form-group">
                                         <label>{{$mission->target}}</label>
