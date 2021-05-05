22fb95541f09cad3106d0e8cba51a526abedeacd
John Chandara <email@john.science>
2021-05-04 22:53:11 -0500

    [view] Overhaul layout into something less horrendeous

--- resources/views/dd20/view.blade.php
+++ resources/views/dd20/view.blade.php
@@ -19,15 +19,19 @@
     <section class="content px-3">
         <div class="row">
             <div class="col-sm-6">
-                <h1><img class="img-thumbnail mr-2" style="width: 4rem" src="{{$player->avatar}}"/><strong>{{$player->name}}</strong></h1>
+                <h1><img class="img-thumbnail mr-2" style="width: 4rem"
+                         src="{{$player->avatar}}"/><strong>{{$player->name}}</strong></h1>
             </div>
         </div>
         <div class="card">
             <div class="card-body">
                 <label for="form-rpc">Website Controls</label>
-                <form id="form-rpc" method="GET" class="form-inline" action="{{ route('dd20.rpc', compact('player')) }}">
+                <form id="form-rpc" method="GET" class="form-inline"
+                      action="{{ route('dd20.rpc', compact('player')) }}">
                     @csrf
-                    <button type="submit" name="action" value="reset_cache" class="btn btn-primary"><i class="fas fa-bomb"></i> Update main cache</button>
+                    <button type="submit" name="action" value="reset_cache" class="btn btn-primary"><i
+                            class="fas fa-bomb"></i> Update main cache
+                    </button>
                 </form>
                 {{--                <label for="form-summary">Summary</label>--}}
                 {{--                <form id="form-summary" class="form-inline">--}}
@@ -58,11 +62,13 @@
         <div class="accordion">
             @foreach($missions as $map => $mapMissions)
                 <div class="card" id="accordion-{{$map}}">
-                    <button class="card-header btn btn-link" id="heading-{{$map}}" data-toggle="collapse" data-target="#card-{{$map}}" aria-expanded="true" aria-controls="collapse">
+                    <button class="card-header btn btn-link" id="heading-{{$map}}" data-toggle="collapse"
+                            data-target="#card-{{$map}}" aria-expanded="true" aria-controls="collapse">
                         <div class="d-flex justify-content-between align-items-center">
                             <div class="d-flex flex-grow-1 align-items-center">
-                                <div class="col col-1">
-                                    <img class="img-thumbnail w-100 h-100" src="//creators.tf/api/mapthumb?map={{$map}}"/>
+                                <div class="col col-2 col-lg-1">
+                                    <img class="img-thumbnail w-100 h-100"
+                                         src="//creators.tf/api/mapthumb?map={{$map}}"/>
                                 </div>
                                 <h2 class="mr-2">{{ucfirst(explode('_', $map)[1])}}</h2>
                                 @foreach($mapMissions as $i => $mission)
@@ -71,43 +77,76 @@
                             </div>
                         </div>
                     </button>
-                    <div id="card-{{$map}}" class="card-body collapse" aria-labelledby="heading" data-parent="#accordion-{{$map}}">
+                    <div id="card-{{$map}}" class="card-body collapse" aria-labelledby="heading"
+                         data-parent="#accordion-{{$map}}">
                         @foreach($mapMissions as $mission)
                             <form action="{{route('dd20.save', compact('player'))}}" method="post" class="mb-5">
                                 @csrf
                                 <input type="hidden" name="reference" value="{{base64_encode($mission->toJson())}}">
                                 <div class="form-group">
-                                    <div class="d-flex align-items-center">
-                                        <h4 class="d-inline"><label>{{$mission->name}}</label></h4>
+                                    <div class="row">
+                                        <div class="col col-2">
+                                            <h4 class="d-inline"><label class="text-truncate">{{$mission->name}}</label>
+                                            </h4>
+                                        </div>
+                                        @if ($mission->waves >= 4)
+                                            <div class="col col-{{$mission->waves - 4}} mr-5"></div>
+                                        @endif
+                                        <div class="col col-md-auto">
+                                            <div class="input-group-sm">
+                                                <button type="submit" name="erase" value="1"
+                                                        data-toggle="tooltip" data-placement="right"
+                                                        title="Reset all progress on this mission (essentially uncheck all)"
+                                                        class="btn btn-sm btn-outline-danger mr-2"><i
+                                                        class="fas fa-eraser"></i></button>
+                                                <button type="button"
+                                                        class="btn btn-sm btn-outline-primary dropdown-toggle dropdown-toggle-split"
+                                                        data-toggle="dropdown" aria-haspopup="true"
+                                                        aria-expanded="false">
+                                                    <i class="fas fa-save"></i> Save Mission
+                                                </button>
+                                                <div class="dropdown-menu">
+                                                    <button type="submit" name="save" value="1"
+                                                            data-toggle="tooltip" data-placement="right"
+                                                            title="The player still has waves to finish"
+                                                            class="btn btn-link dropdown-item">... as Selected
+                                                    </button>
+                                                    <button type="submit" name="loot" value="1"
+                                                            data-toggle="tooltip" data-placement="right"
+                                                            title="The player is done with this mission, auto-mark all waves and award loot."
+                                                            class="btn btn-link dropdown-item">... as Victory (Gives
+                                                        Loot)
+                                                    </button>
+                                                </div>
+                                            </div>
+                                        </div>
                                     </div>
-                                    <div class="form-group row">
+                                    <div class="row">
                                         @isset ($stats[$mission->title])
                                             @php
                                                 $waves = $stats[$mission->title]->waves();
                                             @endphp
                                             @for ($i = 1; $i <= $mission->waves; $i++)
-                                                <div class="col-sm-1">
-                                                    <div class="d-flex flex-column justify-content-center text-center">
-                                                        <label for="{{$mission->title}}.{{$i}}">Wave {{$i}}</label>
-                                                        <input type="checkbox" name="waves[]" value="{{$i}}" {{ isset($waves[$i]) && $waves[$i] ? 'checked' : ''}}>
-                                                    </div>
+                                                <div
+                                                    class="col-sm-1 d-flex flex-column justify-content-center text-center">
+                                                    <label for="{{$mission->title}}.{{$i}}">Wave {{$i}}</label>
+                                                    <input type="checkbox" name="waves[]"
+                                                           value="{{$i}}" {{ isset($waves[$i]) && $waves[$i] ? 'checked' : ''}}>
                                                 </div>
                                             @endfor
                                         @else
                                             @for ($i = 1; $i <= $mission->waves; $i++)
-                                                <div class="col-sm-1">
-                                                    <div class="d-flex flex-column justify-content-center text-center">
-                                                        <label for="{{$mission->title}}.{{$i}}">Wave {{$i}}</label>
-                                                        <input type="checkbox" id="{{$mission->title}}{{$i}}" name="waves[]" value="{{$i}}">
-                                                    </div>
+                                                <div
+                                                    class="col-sm-1 d-flex flex-column justify-content-center text-center">
+                                                    <label for="{{$mission->title}}.{{$i}}">Wave {{$i}}</label>
+                                                    <input type="checkbox" id="{{$mission->title}}{{$i}}"
+                                                           name="waves[]" value="{{$i}}">
                                                 </div>
                                             @endfor
                                         @endisset
-                                    </div>
-                                    <div class="form-group">
-                                        <button type="submit" name="erase" value="1" class="btn btn-default mr-2"><i class="fas fa-eraser"></i> Reset progress</button>
-                                        <button type="submit" name="save" value="1" class="btn btn-default mr-2"><i class="fas fa-save"></i> Save changes</button>
-                                        <button type="submit" name="loot" value="1" class="btn btn-warning"><i class="fas fa-magic"></i> Finish & Distribute Loot</button>
+                                        <div class="col-2">
+                                            <label></label>
+                                        </div>
                                     </div>
                                 </div>
                             </form>
